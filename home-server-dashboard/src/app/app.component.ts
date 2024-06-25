7b2365b2a9e7a7e7296a8f1a24b5a965e551b879
Jannis Brand <jannis.brand@weidmueller.com>
2024-06-25 16:07:55 +0200

    First functionl setup

--- home-server-dashboard/src/app/app.component.ts
+++ home-server-dashboard/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +10,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'home-server-dashboard';
+  constructor(private router: Router) {
+
+  }
+
+   goToHome() {
+      this.router.navigate(['/']);
+   }
+
+   goToAboutMe() {
+    this.router.navigate(['/', 'about']);
+   }
 }
